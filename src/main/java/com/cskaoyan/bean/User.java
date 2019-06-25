4ef82d7023b42e94cd243be4c0aaa4e762e4b9ab
stone4j <shitoushaoxia1993@gmail.com>
2019-06-25 17:48:18 +0800

    commit by idea

---
+++ src/main/java/com/cskaoyan/bean/User.java
@@ -0,0 +1,26 @@
+package com.cskaoyan.bean;
+
+/**
+ * Created by little Stone
+ * Date 2019/6/25 Time 17:45
+ */
+public class User {
+	int id;
+	String username;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
